87806e636c476e08e696f7e5967bacb24710c821
Liu Zechu <liuzechu2013@gmail.com>
2019-06-27 10:38:11 +0800

    trying out MuPDF

--- app/src/main/java/com/gmail/liuzechu2013/singapore/jars/MainActivity.java
+++ app/src/main/java/com/gmail/liuzechu2013/singapore/jars/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -29,13 +31,18 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+// MuPDF viewer
+import com.artifex.mupdf.viewer.DocumentActivity;
 
 public class MainActivity extends AppCompatActivity
         implements ShopFragment.ViewCurrentItemsListener, FilesFragment.OnFileOpenListener{
@@ -221,13 +228,30 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 
-    // TEST: open PDF file
-    @Override
+//    // TEST: open PDF file
+//    @Override
+//    public void openFile() {
+//        Intent intent = new Intent(this, ViewFileActivity.class);
+//        startActivity(intent);
+//    }
+
+    // using MuPDF
     public void openFile() {
-        Intent intent = new Intent(this, ViewFileActivity.class);
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(dir, "Declaration_ReadTheDeclaration.pdf");
+        Uri uri = Uri.fromFile(file);
+
+        startMuPDFActivity(uri);
+    }
+
+    public void startMuPDFActivity(Uri documentUri) {
+        Intent intent = new Intent(this, DocumentActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(documentUri);
         startActivity(intent);
     }
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
