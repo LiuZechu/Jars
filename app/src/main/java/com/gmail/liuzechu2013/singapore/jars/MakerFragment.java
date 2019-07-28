ab4b7c35bf56dd50075187b56ee907524668227a
Liu Zechu <liuzechu2013@gmail.com>
2019-07-28 12:42:15 +0800

    random number generation for maker

--- app/src/main/java/com/gmail/liuzechu2013/singapore/jars/MakerFragment.java
+++ app/src/main/java/com/gmail/liuzechu2013/singapore/jars/MakerFragment.java
@@ -16,6 +16,23 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MakerFragment extends Fragment {
     private ViewCurrentItemsListener activityCommander;
@@ -37,6 +54,12 @@ public class MakerFragment extends Fragment {
     private static final int NUM_MAKER = 3;
     private ViewPager makerPager;
     private PagerAdapter makerPagerAdapter;
+
+    // for generating new items
+    public static final int ORDINARY = 0;
+    public static final int GRAND = 1;
+    public static final int DELUXE = 2;
+
 
     @Override
     public void onAttach(Context context) {
@@ -68,6 +91,32 @@ public class MakerFragment extends Fragment {
         tabLayout.setupWithViewPager(makerPager, true);
         makerPagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager());
         makerPager.setAdapter(makerPagerAdapter);
+
+        // maker buttons
+        Button ordinaryButton = view.findViewById(R.id.maker_page_ordinary_button);
+        ordinaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGeneratingNewItem(ORDINARY);
+            }
+        });
+
+        Button grandButton = view.findViewById(R.id.maker_page_grand_button);
+        grandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGeneratingNewItem(GRAND);
+            }
+        });
+
+        Button deluxeButton = view.findViewById(R.id.maker_page_deluxe_button);
+        deluxeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGeneratingNewItem(DELUXE);
+            }
+        });
+
 
         /*
         // Get all shop items from a .txt file from assets
@@ -186,59 +235,161 @@ public class MakerFragment extends Fragment {
             return NUM_MAKER;
         }
     }
+
+
+    // called when a maker button is clicked
+    private void onGeneratingNewItem(int makerType) {
+        int[] result = generateNewItem(makerType);
+        String fromFile = loadFromLocalFile(CurrentItemsActivity.USER_INVENTORY_FILE_NAME);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<ArrayList<Integer>>>(){}.getType();
+        ArrayList<ArrayList<Integer>> inventory = gson.fromJson(fromFile, type);
+        if (inventory == null) {
+            inventory = new ArrayList<>();
+            inventory.add(new ArrayList<Integer>());
+            inventory.add(new ArrayList<Integer>());
+            inventory.add(new ArrayList<Integer>());
+            inventory.add(new ArrayList<Integer>());
+        }
+        int category = result[0];
+        int item = result[1];
+        ArrayList<Integer> items = inventory.get(0);
+        items.add(item);
+
+        // save changes
+        String toSave = gson.toJson(inventory);
+        saveToLocalFile(CurrentItemsActivity.USER_INVENTORY_FILE_NAME, toSave);
+    }
+
+    // returns an array of 2 integers(int). 1st int represents category, 2nd represents item
+    private int[] generateNewItem(int makerType) {
+        // Random rnd = new Random();
+        // int category = rnd.nextInt(4);
+        int category = ThreadLocalRandom.current().nextInt(1, 4);
+        int item = -1;
+
+        switch (makerType) {
+            case ORDINARY:
+                switch (category) {
+//                    case 0: // colors
+//                        item = ThreadLocalRandom.current().nextInt(min, max + 1);
+//                        break;
+                    case 1: // expressions
+                        item = ThreadLocalRandom.current().nextInt(0, 22 + 1);
+                        break;
+                    case 2: // jars
+                        item = ThreadLocalRandom.current().nextInt(0, 2 + 1);
+                        break;
+                    case 3: // power-ups
+                        item = ThreadLocalRandom.current().nextInt(0, 1 + 1) * 3 + 0;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            case GRAND:
+                switch (category) {
+//                    case 0: // colors
+//                        item = ThreadLocalRandom.current().nextInt(min, max + 1);
+//                        break;
+                    case 1: // expressions
+                        item = ThreadLocalRandom.current().nextInt(23, 44 + 1);
+                        break;
+                    case 2: // jars
+                        item = ThreadLocalRandom.current().nextInt(3, 5 + 1);
+                        break;
+                    case 3: // power-ups
+                        item = ThreadLocalRandom.current().nextInt(0, 1 + 1) * 3 + 1;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            case DELUXE:
+                switch (category) {
+//                    case 0: // colors
+//                        item = ThreadLocalRandom.current().nextInt(min, max + 1);
+//                        break;
+                    case 1: // expressions
+                        item = ThreadLocalRandom.current().nextInt(45, 61 + 1);
+                        break;
+                    case 2: // jars
+                        item = ThreadLocalRandom.current().nextInt(6, 8 + 1);
+                        break;
+                    case 3: // power-ups
+                        item = ThreadLocalRandom.current().nextInt(0, 1 + 1) * 3 + 2;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+             default:
+                 break;
+        }
+
+        int[] result = {category, item};
+
+        return result;
+    }
+
+
+
+    // save a String into local text file on phone
+    public void saveToLocalFile(String fileName, String stringToSave) {
+        FileOutputStream fos = null;
+        try {
+            fos = getActivity(getContext()).openFileOutput(fileName, Context.MODE_PRIVATE);
+            fos.write(stringToSave.getBytes());
+
+            Toast.makeText(getContext(), "changes saved successfully", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // read a string out from local text file
+    public String loadFromLocalFile(String fileName) {
+        FileInputStream fis = null;
+        String output = null;
+
+        try {
+            fis = getActivity(getContext()).openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            output = sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return output;
+    }
 }
-
-
-// for TESTING
-//        superpowerList = new ArrayList<>();
-//        superpowerList.add(new Item(Item.SUPERPOWER, "Life"));
-//        superpowerList.add(new Item(Item.SUPERPOWER, "Happiness"));
-//        superpowerList.add(new Item(Item.SUPERPOWER, "Progress"));
-//        superpowerList.add(new Item(Item.SUPERPOWER, "Democracy"));
-//        superpowerList.add(new Item(Item.SUPERPOWER, "Longevity"));
-//        superpowerList.add(new Item(Item.SUPERPOWER, "Invisibility"));
-//
-//        extensionList = new ArrayList<>();
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//        extensionList.add(new Item(Item.EXTENSION, "extension"));
-//
-//        jarList = new ArrayList<>();
-//        jarList.add(new Item(Item.JAR, "plastic jar"));
-//        jarList.add(new Item(Item.JAR, "porcelain jar"));
-//        jarList.add(new Item(Item.JAR, "bronze jar"));
-//        jarList.add(new Item(Item.JAR, "jelly jar"));
-//        jarList.add(new Item(Item.JAR, "glass jar"));
-//        jarList.add(new Item(Item.JAR, "broken jar"));
-//
-//        candyList = new ArrayList<>();
-//        candyList.add(new Item(Item.CANDY, "sweet candy"));
-//        candyList.add(new Item(Item.CANDY, "sour candy"));
-//        candyList.add(new Item(Item.CANDY, "mala candy"));
-//        candyList.add(new Item(Item.CANDY, "chocolate candy"));
-//        candyList.add(new Item(Item.CANDY, "lollipop"));
-//        candyList.add(new Item(Item.CANDY, "chewing gum"));
-//        candyList.add(new Item(Item.CANDY, "jelly"));
-//
-//        candyExpressionList = new ArrayList<>();
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "happy"));
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "sad"));
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "hesitant"));
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "angry"));
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "crying"));
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "smiley"));
-//        candyExpressionList.add(new Item(Item.CANDY_EXPRESSION, "sarcastic"));
-//
-//        ArrayList<ArrayList<Item>> allShopItems = new ArrayList<>();
-//        allShopItems.add(superpowerList);
-//        allShopItems.add(extensionList);
-//        allShopItems.add(jarList);
-//        allShopItems.add(candyList);
-//        allShopItems.add(candyExpressionList);
-//
-//        Gson gson = new Gson();
-//        String stringConverted = gson.toJson(allShopItems);
